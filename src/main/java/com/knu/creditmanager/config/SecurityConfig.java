ed4fdf7a5eaf55e8f195d1c89b0da9fa42a6b28d
홍석준 <tjrwns0529@gmail.com>
2020-09-15 20:12:38 +0900

    Add jwt config

--- src/main/java/com/knu/creditmanager/config/SecurityConfig.java
+++ src/main/java/com/knu/creditmanager/config/SecurityConfig.java
@@ -1,18 +1,43 @@
 package com.knu.creditmanager.config;
 
+import com.knu.creditmanager.filter.JwtAuthenticationFilter;
+import com.knu.creditmanager.util.JwtUtil;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.servlet.Filter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Value("${jwt.secret}")
+    private String secret;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        Filter jwtFilter = new JwtAuthenticationFilter(authenticationManager(), jwtUtil());
+
         http
                 .cors().disable()
                 .csrf().disable()
                 .formLogin().disable()
-                .headers().frameOptions().disable();
+                .headers().frameOptions().disable()
+                .and()
+                .addFilter(jwtFilter)
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Bean
+    public JwtUtil jwtUtil() {
+        return new JwtUtil(secret);
     }
 }
