ad74ce8278cbb779af7104b7b5f8437053ad078b
홍석준 <tjrwns0529@gmail.com>
2020-11-13 18:07:05 +0900

    Fix: Account CORS

--- src/main/java/com/knu/creditmanager/account/AccountController.java
+++ src/main/java/com/knu/creditmanager/account/AccountController.java
@@ -13,6 +13,7 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 
+@CrossOrigin
 @RequestMapping(value = "/api/accounts")
 @RestController
 @RequiredArgsConstructor
